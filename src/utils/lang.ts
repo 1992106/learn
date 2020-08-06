c3ccd69886af4b26636994d82c0b5136d816c091
wanghuijun <wanghuijun@banggood.com>
2020-08-06 18:23:12 +0800

    feat: call和apply

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -45,8 +45,8 @@ export function toFixed(value: number, length = 2): number {
  * 短横线/下横线/大驼峰 转 小驼峰命名(lowerCamelCase)
  */
 export function getLowerCamelCase(str: string): string {
-  const reg = /(^|-|_)(\w)/g;
-  const reg2 = /^(\w)/g;
+  const reg = /(^|-|_)([A-Za-z0-9])/g;
+  const reg2 = /^([A-Za-z0-9])/g;
   return str.replace(reg, ($, $1, $2) => $2.toUpperCase())
     .replace(reg2, ($, $1) => $1.toLowerCase());
 }
@@ -55,7 +55,7 @@ export function getLowerCamelCase(str: string): string {
  * 短横线/下划线/小驼峰 转 大驼峰命名(UpperCamelCase)
  */
 export function getUpperCamelCase(str: string): string {
-  const reg = /(^|-|_)(\w)/g;
+  const reg = /(^|-|_)([A-Za-z0-9])/g;
   return str.replace(reg, ($, $1, $2) => $2.toUpperCase());
 }
 
@@ -83,4 +83,68 @@ export function getUnderScoreCase(str: string): string {
   return str.replace(reg, ($, $1) => $1.toLowerCase())
     .replace(reg2, ($, $1) => '_' + $1.toLowerCase())
     .replace(reg3, ($, $1) => '_' + $1.toLowerCase());
+}
+
+
+// 防抖
+export function debounce(fn: any, wait: number): any {
+  let timeout: any;
+  return function() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    const args = Array.prototype.slice.call(arguments);
+    timeout = setTimeout(() => {
+      // eslint-disable-next-line no-invalid-this
+      fn.apply(this, args)
+    }, wait)
+  }
+}
+
+// 节流
+export function throttle(fn: any, wait: number): any {
+  let timeout: any;
+  return function() {
+    if (!timeout) {
+    const args = Array.prototype.slice.call(arguments);
+      timeout = setTimeout(() => {
+        timeout = null;
+        // eslint-disable-next-line no-invalid-this
+        fn.apply(this, args)
+      }, wait);
+    }
+  }
+}
+
+// eslint-disable-next-line no-extend-native
+Function.prototype.myCall = function(): any {
+  // let [thisArg, ...args] = [...arguments];
+  // let [thisArg, ...args] = Array.from(arguments);
+  let [thisArg, ...args] = Array.prototype.slice.call(arguments);
+  if (!thisArg) {
+    // context 为 null 或者是 undefined
+    thisArg = typeof window === 'undefined' ? global : window;
+  }
+  // this 的指向的是当前函数 func (func.call)
+  thisArg.fn = this;
+  // 执行函数
+  let result = thisArg.fn(...args);
+  delete thisArg.fn; // thisArg 上并没有 func 属性，因此需要移除
+  return result;
+}
+
+// eslint-disable-next-line no-extend-native
+Function.prototype.myApply = function(): any {
+  // let [thisArg, ...args] = [...arguments];
+  // let [thisArg, ...args] = Array.from(arguments);
+  let [thisArg, args] = Array.prototype.slice.call(arguments);
+  if (!thisArg) {
+      // context 为 null 或者是 undefined
+      thisArg = typeof window === 'undefined' ? global : window;
+  }
+  // this 的指向的是当前函数 func (func.call)
+  thisArg.fn = this;
+  let result = thisArg.fn(...args);
+  delete thisArg.fn; // thisArg 上并没有 func 属性，因此需要移除
+  return result;
 }
