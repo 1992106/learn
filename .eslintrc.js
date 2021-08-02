27c0c789c25667c026a01cb13757f64cbf7fe14e
whj <153104389@qq.com>
2021-08-02 11:25:17 +0800

    feat: memoizeOne.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
@@ -13,16 +10,16 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': ["error", { endOfLine: 'lf' }],
-    "no-undef": "off",
-    "no-unused-vars": "off",
-    "no-redeclare": "off",
-    "no-useless-escape": "off",
-    "no-prototype-builtins": "off",
-    "no-constant-condition": "off",
-    "guard-for-in": "off"
+    'prettier/prettier': ['error', { endOfLine: 'lf' }],
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'no-redeclare': 'off',
+    'no-useless-escape': 'off',
+    'no-prototype-builtins': 'off',
+    'no-constant-condition': 'off',
+    'guard-for-in': 'off'
   }
-}
+};
 
 // "off" or 0 - turn the rule off
 // "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
