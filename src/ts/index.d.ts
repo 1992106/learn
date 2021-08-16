37f012015a532e3121b4c991dd5d9fe25585a1ac
whj <153104389@qq.com>
2021-08-16 11:58:05 +0800

    fix: declare.d

--- src/ts/index.d.ts
+++ src/ts/index.d.ts
@@ -1,7 +1,8 @@
 // interface与type区别
+// interface一般用于声明复杂对象（array、object、function）
 
 /**
- * object
+ * !!!object
  */
 interface Point {
   x: number;
@@ -28,7 +29,7 @@ type Point<T> = {
 };
 
 /**
- * 函数
+ * !!!函数
  */
 interface SetPoint {
   (x: number, y: number): void;
@@ -44,11 +45,19 @@ interface SetPoint {
 type SetPoint = (x: number, y: number) => void;
 // 泛型
 type SetPoint<T> = (x: number, y: number) => T;
+type SetPoint = <T>(x: number, y: number) => T;
+
+// 箭头函数
+// eslint-disable-next-line prettier/prettier
+const arrowFn = <T>(x: T): T => x; // 加逗号（，）的原因是：会和react的jsx语法冲突。
+const arrowFn = <T extends {}>(x: T): T => x;
+const arrowFn = <T extends Record<string, unknown>>(x: T): T => x;
+const arrowFn: <T>(x: T) => T = x => x;
 
 /**
  * 继承
  */
-//  interface extends interface
+// interface extends interface
 interface PartialPointX {
   x: number;
 }
