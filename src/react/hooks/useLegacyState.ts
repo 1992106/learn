b12876ea515cadc9eda709b60d99ee8bf6173508
王会俊 <huijun.wang@patpat.com>
2021-09-22 14:09:35 +0800

    fix: useLegacyState

--- src/react/hooks/useLegacyState.ts
+++ src/react/hooks/useLegacyState.ts
@@ -1,5 +1,6 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
+// 用于合并对象state（useState设置对象时需要解构，使用useLegacyState可以直接设置新对象即可）
 export const useLegacyState = <T>(defaultState: T) => {
   let [state, setState] = useState(defaultState);
 
