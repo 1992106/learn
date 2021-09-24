74b3137d3f10ef69d8be230c682951e8f5690346
王会俊 <huijun.wang@patpat.com>
2021-09-24 17:35:54 +0800

    fix: useEventCallback

--- src/react/hooks/useEventCallback.ts
+++ src/react/hooks/useEventCallback.ts
@@ -2,6 +2,7 @@ import { useRef, useEffect, useCallback } from 'react';
 
 // 从 useCallback 读取一个经常变化的值
 // https://zh-hans.reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback
+// https://zhuanlan.zhihu.com/p/98554943
 function useEventCallback(fn: Function, deps: any[] = []) {
   const ref = useRef(fn);
 
