123d464c0e1d3ec9a4e454f21ea634d6222e2ee4
王会俊 <huijun.wang@patpat.com>
2022-08-01 19:25:02 +0800

    fix: with

--- src/with/index.js
+++ src/with/index.js
@@ -5,11 +5,13 @@ const data = {
 };
 
 // new Function + with
+// new Function传参
 function fnPropChain(data, propChainStr) {
   return new Function('obj', 'with(obj){return' + propChainStr + ';}')(data);
 }
 
 // new Function + with
+// new Function不传参
 function fnPropChain(data, propChainStr) {
   return new Function('with(data){return' + propChainStr + ';}')();
 }
